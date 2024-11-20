287cea83298b7235fead9d0b5c1a0325fd9d222d
nezir <nezir.zahirovic@gmail.com>
2024-11-20 12:16:04 +0800

    ra measures

--- app/controllers/roughness_measurements_controller.rb
+++ app/controllers/roughness_measurements_controller.rb
@@ -1,8 +1,8 @@
 # app/controllers/roughness_measurements_controller.rb
 class RoughnessMeasurementsController < ApplicationController
   layout "dashboard_layout"
-  before_action :set_roughness_measurement, only: [ :show, :edit, :update, :destroy ]
-  before_action :set_delivery_item, only: [ :new, :create ]
+  before_action :set_delivery_item, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_roughness_measurement, only: [:edit, :update, :destroy]
 
   def index
     @roughness_measurements = RoughnessMeasurement.includes(:delivery_item).all
@@ -12,17 +12,19 @@ class RoughnessMeasurementsController < ApplicationController
   end
 
   def new
-    @roughness_measurement = @delivery_item.roughness_measurements.build
+    @roughness_measurement = RoughnessMeasurement.new(delivery_item: @delivery_item)
   end
 
   def edit
+    @roughness_measurement = @delivery_item.roughness_measurement
   end
 
   def create
-    @roughness_measurement = @delivery_item.roughness_measurements.build(roughness_measurement_params)
+    @roughness_measurement = RoughnessMeasurement.new(roughness_measurement_params)
+    @roughness_measurement.delivery_item = @delivery_item
 
     if @roughness_measurement.save
-      redirect_to @roughness_measurement, notice: "Roughness measurement was successfully created."
+      redirect_to delivery_item_path(@delivery_item), notice: "Roughness measurement was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -30,7 +32,7 @@ class RoughnessMeasurementsController < ApplicationController
 
   def update
     if @roughness_measurement.update(roughness_measurement_params)
-      redirect_to @roughness_measurement, notice: "Roughness measurement was successfully updated."
+      redirect_to delivery_item_path(@roughness_measurement.delivery_item), notice: "Roughness measurement was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
@@ -43,12 +45,12 @@ class RoughnessMeasurementsController < ApplicationController
 
   private
 
-  def set_roughness_measurement
-    @roughness_measurement = RoughnessMeasurement.find(params[:id])
-  end
-
   def set_delivery_item
     @delivery_item = DeliveryItem.find(params[:delivery_item_id])
+  end
+
+  def set_roughness_measurement
+    @roughness_measurement = RoughnessMeasurement.find_by!(delivery_item_id: @delivery_item.id)
   end
 
   def roughness_measurement_params
--- app/models/delivery_item.rb
+++ app/models/delivery_item.rb
@@ -1,9 +1,10 @@
 class DeliveryItem < ApplicationRecord
   belongs_to :incoming_delivery
   has_many :quality_inspections, dependent: :destroy
-  has_one :roughness_measurement, dependent: :destroy
+  has_many :roughness_measurements, dependent: :destroy
   has_many :material_certificate_items, dependent: :destroy
   has_many :material_certificates, through: :material_certificate_items
+  has_one :roughness_measurement
 
   validates :tag_number, presence: true, uniqueness: { scope: :incoming_delivery_id }
   validates :batch_number, presence: true
