f6f9f43e6a65a616663593330c2c61faefe8ce18
nezir <nezir.zahirovic@gmail.com>
2024-11-21 14:12:26 +0800

    fixing flags issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,3 +17,4 @@ Rails.application.config.assets.paths << Rails.root.join("app/assets/images/flag
 # Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
 Rails.application.config.assets.precompile << "bootstrap.min.js"
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
+Rails.application.config.assets.precompile += %w( *.svg )
