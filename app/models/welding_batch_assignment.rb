3c54eb7030f9522847b8cf4baa484fdc25eaee8e
nezir <nezir.zahirovic@gmail.com>
2025-01-14 17:20:52 +0800

    updates

--- app/models/welding_batch_assignment.rb
+++ app/models/welding_batch_assignment.rb
@@ -2,7 +2,7 @@ class WeldingBatchAssignment < ApplicationRecord
   belongs_to :work_preparation
   belongs_to :welding
   belongs_to :material_certificate, optional: true
-  belongs_to :material_certificate1, class_name: 'MaterialCertificate', optional: true
+  belongs_to :material_certificate1, class_name: "MaterialCertificate", optional: true
 
   validates :welding_id, presence: true
 end
--- app/models/work_preparation.rb
+++ app/models/work_preparation.rb
@@ -18,7 +18,7 @@ class WorkPreparation < ApplicationRecord
     attachable.variant :medium, resize_to_limit: [ 1200, 1200 ]
   end
 
-  validates :work_package_number, presence: true # , uniqueness: { scope: :project_id }
+  validates :work_package_number, presence: true, uniqueness: { scope: :project_id }
   validates :on_hold_status, inclusion: { in: ON_HOLD_STATUSES, allow_nil: true }
   validates :on_hold_comment, length: { maximum: 2000 }
   validates :work_preparation_type, presence: true
