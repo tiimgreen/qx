35df02e866c60273f6e678e0147b992b65a42b70
nezir <nezir.zahirovic@gmail.com>
2025-01-16 15:47:08 +0800

    adding guests views

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,15 +1,14 @@
 class Guest < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
+  # Include only necessary devise modules
+  devise :database_authenticatable,
+         :rememberable, :validatable
+
   belongs_to :project
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :active, inclusion: { in: [true, false] }
-  
+  validates :active, inclusion: { in: [ true, false ] }
+
   def full_name
     "#{first_name} #{last_name}"
   end
