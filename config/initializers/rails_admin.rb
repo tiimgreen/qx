a4e80c5791f649634680e4140e214e6db80ff5c3
nezir <nezir.zahirovic@gmail.com>
2025-01-16 17:25:34 +0800

    fix bug on rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -49,9 +49,6 @@ RailsAdmin.config do |config|
 
     list do
       field :user do
-        formatted_value do
-          bindings[:object].user ? "#{bindings[:object].user.first_name} #{bindings[:object].user.last_name}" : "No user"
-        end
       end
       field :sector
       field :created_at
@@ -61,6 +58,44 @@ RailsAdmin.config do |config|
     edit do
       field :user
       field :sector
+    end
+  end
+
+  config.model "Isometry" do
+    edit do
+      field :project
+      field :sector
+      field :user
+      field :weldings
+      field :isometry_documents
+      field :material_certificates do
+        associated_collection_cache_all false
+        associated_collection_scope do
+          proc { |scope| scope.search_by_term(query) }
+        end
+      end
+      field :received_date
+      field :pid_number
+      field :pid_revision
+      field :line_id
+      field :dn
+      field :revision_number
+      field :revision_last
+      field :page_number
+      field :page_total
+      field :medium
+      field :pipe_length
+      field :workshop_sn
+      field :assembly_sn
+      field :total_sn
+      field :total_supports
+      field :total_spools
+      field :rt
+      field :vt2
+      field :pt2
+      field :on_hold_date
+      field :on_hold_status
+      field :qr_position
     end
   end
 end
