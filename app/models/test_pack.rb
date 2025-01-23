08378f81c10ccadde847123792778d8b5f66be79
nezir <nezir.zahirovic@gmail.com>
2025-01-23 10:48:09 +0800

    relaksed validation test_pack

--- app/models/test_pack.rb
+++ app/models/test_pack.rb
@@ -13,7 +13,7 @@ class TestPack < ApplicationRecord
     attachable.variant :medium, resize_to_limit: [ 1200, 1200 ]
   end
 
-  validates :work_package_number, presence: true, uniqueness: { scope: [ :project_id, :isometry_id ] }
+  validates :work_package_number, presence: true, uniqueness: { scope: [ :project_id, :isometry_id, :test_pack_type ] }
   validates :on_hold_status, inclusion: { in: ON_HOLD_STATUSES, allow_nil: true }
   validates :on_hold_comment, length: { maximum: 2000 }
   validates :test_pack_type, presence: true
