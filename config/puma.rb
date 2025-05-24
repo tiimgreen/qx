f6879cc5e7ca9ffd32a4b6ba1349a3be409e2573
nezir <nezir.zahirovic@gmail.com>
2025-05-24 12:40:11 +0800

    config: enhance puma server configuration with production and development environments

--- config/puma.rb
+++ config/puma.rb
@@ -1,34 +1,54 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# Determine the Rails environment
+rails_env = ENV.fetch("RAILS_ENV", "production")
+environment rails_env
 
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+# Common configuration
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Environment-specific configuration
+case rails_env
+when "production"
+  # Production-specific settings
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Allow puma to be restarted by `bin/rails restart` command.
+
+  app_dir = File.expand_path("../..", __FILE__)
+  shared_dir = "#{app_dir}/shared"
+
+  # Set socket permissions
+  unix_socket_permissions = 0660
+
+  # Use a UNIX socket for production
+  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+  # Uncomment the following line if you need TCP binding as well
+  bind "tcp://0.0.0.0:3050"
+
+  # Specify the pidfile location
+  pidfile "#{shared_dir}/tmp/pids/puma.pid"
+  state_path "#{shared_dir}/tmp/pids/puma.state"
+
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+  # Preload the application for performance
+  preload_app!
+
+when "development"
+  # Development-specific settings
+  worker_timeout 3600
+  # Listen on port 3000 by default
+  port ENV.fetch("PORT", 3000)
+end
+
+# Common settings for all environments
 plugin :tmp_restart
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+# Optionally, you can add the following for better error handling:
+lowlevel_error_handler do |e|
+  Raven.capture_exception(e)
+  [ 500, {}, [ "An error has occurred, and engineers have been informed. Please reload the page. If you continue to have problems, contact support@example.com\n" ] ]
+end
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
