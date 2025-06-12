8fbe031219b7e79ceeed5b6bc2472f9d4252abec
nezir <nezir.zahirovic@gmail.com>
2025-06-12 16:10:13 +0800

    config: update production host to qxd-qualinox-rohrleitungsbau.ch domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   routes.default_url_options = {
-    host: "qx.aiplace.ltd",
+    host: "qxd-qualinox-rohrleitungsbau.ch",
     protocol: "https"
   }
 end
